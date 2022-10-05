a73c9ab3b839a688f2a041ed0ed59c7589064961
BLO-38 <lars3838@gmail.com>
2022-10-05 11:40:30 +0200

    Initial commit

---
+++ src/paket1/AndraKlassen.java
@@ -0,0 +1,7 @@
+package paket1;
+
+public class AndraKlassen {
+    public static void main(String[] args) {
+        System.out.println("2a");
+    }
+}
---
+++ src/paket1/HelloRosendal.java
@@ -0,0 +1,7 @@
+package paket1;
+
+public class HelloRosendal {
+    public static void main(String[] args) {
+        System.out.println("Hej Rosewndal");
+    }
+}
