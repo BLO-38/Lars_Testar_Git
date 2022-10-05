73d1c01c9eb40c10b6964881aca0f6a2f1a391ed
Lars Blomgren <53094710+BLO-38@users.noreply.github.com>
2022-10-05 11:46:56 +0200

    Update AndraKlassen.java

--- src/paket1/AndraKlassen.java
+++ src/paket1/AndraKlassen.java
@@ -4,5 +4,7 @@ public class AndraKlassen {
     public static void main(String[] args) {
         System.out.println("2a");
         System.out.println("Ny kod");
+        System.out.println("Utskrift tillagd i git");
+        
     }
 }
