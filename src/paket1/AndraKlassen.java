adb711cf6c4083bf502a7146d0a1bbf8d9f89f06
Lars Blomgren <lars3838@gmail.com>
2022-10-05 21:51:32 +0200

    commit d2 test master

--- src/paket1/AndraKlassen.java
+++ src/paket1/AndraKlassen.java
@@ -4,15 +4,14 @@ public class AndraKlassen {
     public static void main(String[] args) {
         System.out.println("2a");
         System.out.println("Ny kod");
-        System.out.println("Utskrift tillagd i git");
-        System.out.println("Utskrift");
-        System.out.println("2a");
-        System.out.println("Ny kod");
-        System.out.println("Nytt igen i webben");
+        //BORTA
         System.out.println("Detta skrevs hemma");
         System.out.println("Hemma igen");
         // KOMMENTAR!!
         // NU SKRIVER JAG OCH PUSAHAR TILL LARBLO1
         
+    }
+    public static void metodMedBLO38 () {
+        System.out.println("En metod! med BLO-38");
     }
 }
